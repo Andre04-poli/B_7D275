85a138b6cfdc053a3d3b5d24be7d65075ca7a389
Andrea04-poli <andrea2.bruno@mail.polimi.it>
2024-02-07 03:41:56 +0100

    first commit

--- src/main/java/CodekataProblem.java
+++ src/main/java/CodekataProblem.java
@@ -3,7 +3,7 @@ public class CodekataProblem {
      * Here write functions to be implemented
      * */
     public static int myProblem1(int i, int j){
-        return i;
+        return i+i-i;
     }
 
     public static int myProblem2(int i, int j){
